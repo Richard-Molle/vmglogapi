6bbcf968342dc4cc293dac0cd8cd1269c354526c
reguinard <remiguinard@protonmail.ch>
2019-03-05 13:08:32 +0100

    first commit

---
+++ src/main/java/fr/ipme/vmglogapi/VmglogapiApplication.java
@@ -0,0 +1,13 @@
+package fr.ipme.vmglogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmglogapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmglogapiApplication.class, args);
+	}
+
+}
