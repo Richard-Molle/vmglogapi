715914851ad25b908a3bef226afec9a7fd556adc
Richard <richard.molle27@gmail.com>
2019-03-13 16:20:30 +0100

    Modification du front + ajout de la méthode de trie des logs

--- front/affichageLog.js
+++ front/affichageLog.js
@@ -1,1 +1,42 @@
- 
+function affichageLog(){
+	var typeLog = document.querySelector("input[name='typeLog']:checked").value;
+	var myHeaders = new Headers();
+
+	var myInit = {
+		method: 'POST',
+		header: myHeaders
+//		mode: 'no-cors'
+	};
+
+	var myRequest = new Request('http://localhost:8080/api/logs/' + typeLog, myInit);
+
+
+    fetch(myRequest, myInit)
+	// .then(function(response){ //response => response.json())
+	// .then(response => response.json())
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(myLogs) {
+		//var codeHTML = "";
+		var listLogs=document.getElementById('spanLogs');
+		var p = document.createElement('p');
+
+		/*p.innerText = 'truyugsd';
+		p.classList.add('test');
+		listLogs.appendChild(p);*/
+		for(var i=0; i < myLogs.length; i++){
+			p.innerText = myLogs[i].id + ': ' + myLogs[i].contentLog;
+			listLogs.appendChild(p);
+			//codeHTML +='<p>' + myLogs[i].id + ': ' + myLogs[i].contentLog + '</p><br/>';
+			
+		}
+		//listLogs.innerHTML= codeHTML;
+	})
+//	.catch(err => {
+//  		console.log(err);
+//	})
+	;
+
+	return false;
+}
--- src/main/java/fr/ipme/vmglogapi/controller/LogController.java
+++ src/main/java/fr/ipme/vmglogapi/controller/LogController.java
@@ -5,7 +5,10 @@ import fr.ipme.vmglogapi.repository.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api/logs")
@@ -22,6 +25,25 @@ public class LogController {
     @GetMapping(value = "/{id}")
     public Log get(@PathVariable int id){
         return logRepository.getOne(id);
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/{typeLog}")
+    public List<Log> readLogs(@PathVariable String typeLog){
+        Matcher m;
+        List<Log> listLog = new LinkedList<>();
+        Log log;
+
+        List<Log> logs = list();
+        Pattern p = Pattern.compile(".*"+typeLog+".*");    /////////////////////////////////////////////
+        for (int i=0; i<logs.size();i++){
+            log = logs.get(i);
+            m= p.matcher(log.getContentLog());
+            if (m.find()){
+                listLog.add(log);
+            }
+        }
+        return listLog;
     }
 
     @PostMapping
