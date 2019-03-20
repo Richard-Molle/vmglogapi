1d9857fdc9e98cf7fdb5fc194ef6692c9f80f607
Richard <richard.molle27@gmail.com>
2019-03-20 10:56:23 +0100

    Commentaire de la classe de test initiale

--- src/test/java/fr/ipme/vmglogapi/VmglogapiApplicationTests.java
+++ src/test/java/fr/ipme/vmglogapi/VmglogapiApplicationTests.java
@@ -1,4 +1,4 @@
-package fr.ipme.vmglogapi;
+/*package fr.ipme.vmglogapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class VmglogapiApplicationTests {
 	}
 
 }
+*/
